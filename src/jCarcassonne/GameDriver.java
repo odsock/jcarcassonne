191319273dbaf232664a9bdcd310f8ae765e6d92
odsock <odsock@52e5ef46-0093-11de-ac65-f74f249cfaac>
2009-03-03 15:17:56 +0000

    placement rules, basic display, random test placement

--- src/jCarcassonne/GameDriver.java
+++ src/jCarcassonne/GameDriver.java
@@ -2,7 +2,6 @@ package jCarcassonne;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
@@ -17,35 +16,54 @@ import javax.swing.JPanel;
 
 public class GameDriver {
 
-	Stack<Tile> tileStack = new Stack<Tile>();
-	Landscape landscape;
-	Player[] players = new Player[2];  //change this later to accommodate more players
+	private Stack<Tile> tileStack = new Stack<Tile>();
+	private Landscape landscape;
+	private Player[] players = new Player[2];  //change this later to accommodate more players
 
 	public static void main(String[] args) {
 		GameDriver game = new GameDriver();
-		
+
 		//load and randomize the tileset
 		game.fillStack();
-		
+
 		//seed the landscape with the start tile
 		game.landscape = new Landscape(game.tileStack.pop());
-		
+
 		//add some more tiles for testing
-		game.landscape.placeTile(game.tileStack.pop(), new Point(0,1));
-		System.out.println("Driver: " + game.landscape.getTile(new Point(0,1)));
-		
+		Random rand = new Random();
+		int i = 0;
+		int j = 0;
+		while(i < 1000 && !game.tileStack.empty())
+		{
+			int x = rand.nextInt(10);
+			int y = rand.nextInt(10);
+			if(Rules.checkPlacement(game.landscape, game.tileStack.peek(),x,y))
+			{
+				game.landscape.placeTile(game.tileStack.pop(), x,y);
+				j = 0;
+			}
+			i++;
+			j++;
+			if(j > 9)
+			{
+				game.tileStack.pop();
+//				System.out.println("pop");
+				j = 0;
+			}
+		}
+
 		//setup the players
 		game.players[0] = new Player("Player1", Color.red);
 		game.players[1] = new Player("Player2", Color.blue);
-		
+
 		//try to display some stuff
 		JFrame f = new JFrame();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel p = new JPanel(new BorderLayout());
 		p.add(game.landscape, BorderLayout.CENTER);
 		f.add(p);
-//		ImageIcon ii = new ImageIcon(game.tileStack.pop().getImage());
-//		p.add(new JLabel(ii));
+		//		ImageIcon ii = new ImageIcon(game.tileStack.pop().getImage());
+		//		p.add(new JLabel(ii));
 		f.pack();
 		f.setVisible(true);
 	}
@@ -56,25 +74,25 @@ public class GameDriver {
 			while(in.ready()){
 				String filename = in.readLine().split(" ")[1];
 				BufferedImage img = ImageIO.read(new File(filename));
-				
+
 				int count = Integer.parseInt(in.readLine().split(" ")[1]);
-				
+
 				in.readLine();
-				
+
 				Tile.Feature[] features = new Tile.Feature[5];
 				for(int i = 0; i < 4; i++){
 					features[i] = Tile.Feature.valueOf(in.readLine().split(" ")[1]);
 				}
 				in.readLine();
-				
+
 				for(int i = 0; i < count; i++)
-					templist.add(new Tile(features[0], features[1], features[2], features[3], features[4], img));
+					templist.add(new Tile(features[0], features[1], features[2], features[3], features[4], img, filename));
 			}
 		} 
 		catch (IOException e) {
 			System.out.println("Error reading tile set.\n" + e);
 		}
-		
+
 		//randomize stack from list of tiles loaded
 		//keep start tile at head of stack
 		Tile starttile = templist.remove(0);
